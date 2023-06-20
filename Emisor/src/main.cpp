acd692343d8f872e44b305a5295d848e71b2a666
nandoelmeromero <fcj199914@gmail.com>
2023-06-20 14:40:56 -0600

    fix:Codigo funcionando. Envio de señales por infrarojo

--- Emisor/src/main.cpp
+++ Emisor/src/main.cpp
@@ -7,7 +7,7 @@
 
 #define IRSND_SUPPORT_NEC_PROTOCOL  1
 #define IRSND_PROTOCOL_NAMES        1
-#define IRSND_OUTPUT_PIN            PA7            //Pin por donde mandare la señal de salida
+#define IRSND_OUTPUT_PIN            PA7            
 
 //Librerias necesarias para el manejo de la pantalla OLED
 #include <Wire.h>
@@ -21,6 +21,7 @@
 //Declaracion de funciones auxiliares
 void mandarValor();
 void conmutar();
+int sending();
 
 const int buttonPin1 = PA5;  // Pin del primer botón
 const int buttonPin2 = PA6;  // Pin del segundo botón
@@ -29,6 +30,8 @@ const int Pin3 = PA1;        // Pin del led
 int valorPotenciometro;
 int voltajeEnviar;           // Voltaje en porcentaje
 int mode = 0;
+
+int sendValue;
 
 int porcentaje;
 float voltaje;
@@ -45,34 +48,33 @@ IRMP_DATA irsnd_data;
 
 void setup(){
 
-  //ENVIO DEL VALOR
+  //IRSND
   irsnd_data.protocol = IRSND_SUPPORT_NEC_PROTOCOL;    // Protocolo utilizado
   irsnd_data.address = 0x02;                         // Dirección de la placa emisora
   irsnd_data.flags = 0;
 
   irsnd_init();
 
-  pinMode(buttonPin1, INPUT_PULLUP);    // Configura el primer botón como entrada con resistencia pull-up interna
-  pinMode(buttonPin2, INPUT_PULLUP);    // Configura el segundo botón como entrada con resistencia pull-up interna
+  pinMode(buttonPin1, INPUT_PULLUP);              // Configura el primer botón como entrada con resistencia pull-up interna
+  pinMode(buttonPin2, INPUT_PULLUP);              // Configura el segundo botón como entrada con resistencia pull-up interna
 
-  pinMode(Pin3, OUTPUT);                // Configura el Pin3 como salida
+  pinMode(Pin3, OUTPUT);                          // Configura el Pin3 como salida
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Inicializa la pantalla OLED
-  display.setTextColor(SSD1306_WHITE); // Configura el color del texto en blanco
-  display.setTextSize(1); // Configura un tamaño de texto pequeño
-  display.clearDisplay(); // Limpia la pantalla
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);      // Inicializa la pantalla OLED
+  display.setTextColor(SSD1306_WHITE);            // Configura el color del texto en blanco
+  display.setTextSize(1);                         // Configura un tamaño de texto pequeño
+  display.clearDisplay();                         // Limpia la pantalla
 
-  digitalWrite(Pin3, LOW);    //El led inicia apagado
+  digitalWrite(Pin3, LOW);                        //El led inicia apagado
 }
 
 void loop(){
 
   //AQUI SE LEE EL VALOR DEL POTENCIOMETRO
   valorPotenciometro = analogRead(PA0);
-  voltaje = valorPotenciometro * (3.3 / 1023); //Se transforma el valor del potenciometro a voltaje
-  porcentaje = map(valorPotenciometro, 0, 1028, 0, 100); //Porcentaje del potenciometro
+  voltaje = valorPotenciometro * (3.3 / 1023);            //Se transforma el valor del potenciometro a voltaje
+  porcentaje = map(valorPotenciometro, 0, 1028, 0, 100);  //Porcentaje del potenciometro
   
-
   display.clearDisplay();
   display.setCursor(10, 10);
   display.print("\n");
@@ -89,17 +91,14 @@ void loop(){
     display.print("\n");
     display.print("\n");
     display.print("MODO APAGADO ");
-    display.print(mode);
   }else if(mode == 1){
     display.print("\n");
     display.print("\n");
     display.print("MODO ENCENDIDO ");
-    display.print(mode);
   }else if(mode == 2){
     display.print("\n");
     display.print("\n");
     display.print("MODO ENCENDIDO Y REGULADO");
-    display.print(mode);
   }
 
   display.display();
@@ -114,44 +113,41 @@ void loop(){
     if(buttonState1){
       mandarValor();
     }
-    
   }
 
   if(newButtonState2 != buttonState2){
-    
-    buttonState2 = newButtonState2; 
-
+    buttonState2 = newButtonState2;
     if(buttonState2){  
       conmutar();
     }
-  
   }
 
+  //Modo de trabajo
   if(mode == 0){
     analogWrite(Pin3, 0);
-  
   }else if(mode == 1){
     analogWrite(Pin3, 1);
-  
   }else if(mode == 2){
-    //int brillo = map(voltaje, 0, 3.3, 0, 255);
     analogWrite(Pin3, voltaje);
   }
 
-
 }
 
+//Funcion que envia por infrarojo
 void mandarValor(){
 
   if(newButtonState1){
     
-    irsnd_data.command = mode;
-    irsnd_send_data(&irsnd_data, true);                 // Envio la data
+    sendValue = sending();
+
+    irsnd_data.command = sendValue;
+
+    irsnd_send_data(&irsnd_data, true);                         // Envio la data
   
     delay(100);                                       
-    display.clearDisplay();                           // Limpia la pantalla
-    display.setCursor(0, 0);                          // Establece la posición del cursor
-    display.println("Se enviara la siguiente informacion:");         // Imprime el texto inicial en la pantalla
+    display.clearDisplay();                                     // Limpia la pantalla
+    display.setCursor(0, 0);                                    // Establece la posición del cursor
+    display.println("Se enviara la siguiente informacion:");    // Imprime el texto inicial en la pantalla
     display.print("Modo: ");
     display.print(mode);
     display.print("\n");
@@ -168,38 +164,29 @@ void mandarValor(){
 
 }
 
+//Funcion que cambia el modo de trabajo (apagado, encendido o controlado)
 void conmutar(){
   
   if(mode == 0){
-    /* display.clearDisplay();                           // Limpia la pantalla
-    display.setCursor(0, 0);                          // Establece la posición del cursor
-    display.println("LED APAGADO");                   // Imprime el texto inicial en la pantalla
-    display.println(mode);                            // Imprime el texto inicial en la pantalla
-    display.display(); */
     mode++;
   }else if(mode == 1){
-    
-    /* display.clearDisplay();                           // Limpia la pantalla
-    display.setCursor(0, 0);                          // Establece la posición del cursor
-    display.println("LED ENCENDIDO");                   // Imprime el texto inicial en la pantalla
-    display.println(mode);                            // Imprime el texto inicial en la pantalla
-    display.display(); */
     mode++;
-  
   }else if(mode == 2){
-    
-    /* display.clearDisplay();                           // Limpia la pantalla
-    display.setCursor(0, 0);                          // Establece la posición del cursor
-    display.println("LED ENCENDIDO Y REGULADO");                   // Imprime el texto inicial en la pantalla
-    display.println(mode);                            // Imprime el texto inicial en la pantalla
-    display.display(); */
-
     mode = 0;
-    
-    //int brillo = map(voltaje, 0, 3.3, 0, 255); // Convierte el valor de voltaje a un entero entre 0 y 255
-    //analogWrite(Pin3, brillo); //Este valor solo se actualiza si el led esta prendido y no en "tiempo real"
-    //Hay que encontrar la forma de que lo haga en tiempo real.
   }
 
   delay(1000);
+}
+
+//Funcion que retorna el modo de trabajo
+int sending(){
+
+  if(mode == 0){
+    return 0;
+  }else if(mode == 1){
+    return 1;
+  }else if (mode == 2){
+    return porcentaje;
+  }
+
 }
